aa32b7bd8a9d87ded154888c4ac43834ca712781
yshm0205 <yshm0205@gmail.com>
2026-01-16 20:39:08 +0900

    fix: change default locale from en to ko

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -6,7 +6,7 @@ export const routing = defineRouting({
   locales: ["en", "ko"],
 
   // Used when no locale matches
-  defaultLocale: "en",
+  defaultLocale: "ko",
 });
 
 // Lightweight wrappers around Next.js' navigation APIs
